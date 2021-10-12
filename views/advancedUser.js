1545ac94364d4f09ff4a590858af8ba46481972f
Chanpreet24 <62950691+Chanpreet24@users.noreply.github.com>
2021-10-12 21:18:04 +0100

    advanced user html and advanced user logic updated

---
+++ views/advancedUser.js
@@ -0,0 +1,61 @@
+window.onload = function(){
+    linkCategorytoDB()
+};
+
+document.querySelector('#log-out-button').addEventListener('click', () => {
+    window.location.href = './loginPage.html';
+});
+
+
+var option = "";
+
+
+function linkCategorytoDB (){
+    var categories = [];
+     db.allDocs({
+        include_docs: true
+      }).then(function (result) {
+       var dbList = result.rows;
+       dbList.forEach(element => {
+           if(!categories.includes(element.doc.quizName))
+           {
+            categories.push(element.doc.quizName)
+           }
+       })
+            for(var i =0; i< categories.length; i++)
+            {
+                option += '<option value="' + categories[i] + '">' + categories[i] + "</option>"
+            }
+                document.getElementById('quizFormControl').innerHTML = option
+                
+         });
+    }
+
+
+    function getdetails(e){
+        e.preventDefault();
+        var e = document.getElementById("quizFormControl");
+        var quizCategoryName = e.value;
+    db.allDocs({
+            include_docs: true
+          }).then(function (result) {
+           var quizName = result.rows;
+           $("#quizTable").find("tr:not(:first)").remove();
+           quizName.forEach(element => {
+            if(element.doc.quizName == quizCategoryName)
+            var row = "<tr><td>" + element.doc.quizName + "</td><td>" + element.doc.question + "<td>" + element.doc.answer1 + "</td>"+ "<td>" + element.doc.answer2 + "</td>"+ "<td>" + element.doc.answer3 + "</td>"+ "<td>" + element.doc.answer4 + "</td>" + "<td>" + element.doc.correctanswer;
+               $("#quizTable").append(row);
+           });
+          }).catch(function (err) {
+            console.log(err);
+          });
+    }
+    
+    $('#quizFormControl').on('change', getdetails);
+
+
+//I need to dynamically add the quizzes into this array from the quiz db, based on the quiz name stored in the db
+
+
+
+//On the click of the option, the questions and answers need to be displayed in a table
